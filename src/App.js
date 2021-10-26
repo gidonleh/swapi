106c1706432be8a05c9fd9995bac02b3366e5930
Gadileh <csgadileh@gmail.com>
2021-10-26 21:23:29 +0300

    modifying package.json and changing BrowserRouter to HashRouter in order to publish project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { BrowserRouter as Router, Link, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Link, Switch, Route } from 'react-router-dom';
 import Sidebar from './components/Sidebar'
 import Main from './components/Main'
 import { Dimmer, Loader } from 'semantic-ui-react';
